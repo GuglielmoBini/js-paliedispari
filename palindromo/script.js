4ed96c18095c080fe053c5e8131c22aa372a8c59
GuglielmoBini <guglielmobini@gmail.com>
2022-12-15 16:26:12 +0100

    completato struttura html e step preliminari

--- palindromo/script.js
+++ palindromo/script.js
@@ -1,1 +1,19 @@
-console.log("js-1 ok");
+/*
+Palindroma
+
+- Chiedere all’utente di inserire una parola tramite un form.
+- Creare una funzione per capire se la parola inserita è palindroma.
+*/
+
+//---------------------------------------------------------------
+
+/*
+1- prendo gli elementi dal DOM.
+2- aggiungo event listener al form.
+3- prendo il value dell'input.
+4- creo una funzione che ribalti la parola scritta dall'utente.
+5- controllo se la parola ribaltata è uguale all'originale.
+5a- se si (vero), la parola è palindroma.
+5b- se no(falso), la parola non è palindroma. 
+6- stampo il risultato in pagina.
+*/
