bb00358fa6ea79876dcaa9f7b1ed22c5e260d6cf
GuglielmoBini <guglielmobini@gmail.com>
2022-12-15 23:32:27 +0100

    create funzioni, completato event listener, stampo in pagina

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -15,7 +15,7 @@ Pari e Dispari
 2- creo event listener al bottone
 3- creo una funzione che generi un numero random da 1 a 5
 4- sommo i numeri
-5- creo una funzione che stabilisca se la somma dei due numeri è pari o dispari
+5- creo una funzione che stabilisca se la somma dei due numeri è pari
 6- dichiaro chi ha vinto
 7- stampo in pagina
 */
@@ -24,21 +24,70 @@ Pari e Dispari
 
 // prendo gli elementi dal DOM
 const targetElement = document.getElementById("target");
-const choose = document.getElementById("choose");
-const number = document.getElementById("number");
+const inputChoose = document.getElementById("choose");
+const inputNumber = document.getElementById("number");
 const cpuNumber = document.getElementById("cpu-number");
 const submitButton = document.getElementById("submit");
+const sumNumbers = document.getElementById("sum");
 
 //creo event listener al bottone
-
 submitButton.addEventListener("click", function () {
-  const userChoose = choose.value.trim();
-  const userNumber = number.value.trim();
+  // prendo i value degli input
+  const userChoose = inputChoose.value;
+  const userNumber = parseInt(inputNumber.value.trim());
+
   console.log(userChoose, userNumber);
+
+  //preparo variabili d'appoggio
+  let cpuRandomNumber = 0;
+  let result = "Hai perso";
+  let sum = 0;
+
+  // validazione
+  if (isNaN(userNumber) || userNumber < 1 || userNumber > 5) {
+    alert("Devi inserire un numero tra 1 e 5!");
+  } else if (!userChoose) {
+    alert("Devi scegliere pari o dispari");
+  } else {
+    cpuRandomNumber = getRandomNumber(1, 5);
+
+    console.log(cpuRandomNumber);
+
+    // sommo i numeri
+    sum = userNumber + cpuRandomNumber;
+
+    console.log(sum);
+
+    // dichiaro chi ha vinto
+    if (
+      (isEven(sum) && userChoose === "even") ||
+      (!isEven(sum) && userChoose === "odd")
+    ) {
+      result = "Hai vinto!";
+    }
+
+    console.log(result);
+  }
+  // stampo in pagina
+  cpuNumber.innerText = cpuRandomNumber;
+  sumNumbers.innerText = sum;
+  targetElement.innerText = result;
 });
 
+// --------FUNZIONI--------
+
 // creo una funzione che generi un numero random da 1 a 5
-function getRandomNumber() {
-  const randomNumber = Math.floor(Math.random() * 5) + 1;
+function getRandomNumber(min, max) {
+  const randomNumber = Math.floor(Math.random() * max - min + 1) + min;
   return randomNumber;
+}
+
+// creo una funzione che stabilisca se la somma dei due numeri è pari
+
+function isEven(number) {
+  let isEven = false;
+  if (number % 2 === 0) {
+    isEven = true;
+  }
+  return isEven;
 }
